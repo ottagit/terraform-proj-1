8f955cf99b9a7520d994977fec9cc1aa4fb59ba4
ottagit <ottaosawa@gmail.com>
2023-02-28 03:52:35 +0000

    deploy a configurable web server

--- main.tf
+++ main.tf
@@ -2,11 +2,53 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0912f71e06545ad88"
-  instance_type = "t2.micro"
+  ami                    = "ami-0912f71e06545ad88"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "tf-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "tf-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf-sg"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
