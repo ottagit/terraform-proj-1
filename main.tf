91c916e7f9ebefc2c8f7f474ff7e2852102f0db7
ottagit <ottaosawa@gmail.com>
2023-03-01 13:09:51 +0000

    create an application load balancer

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
+# Define your Terraform provider
 provider "aws" {
   region = "ap-south-1"
 }
 
+# Define variables for use in your configuration
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
 
+# Create an example EC2 instance
 resource "aws_instance" "example" {
   ami                    = "ami-0912f71e06545ad88"
   instance_type          = "t2.micro"
@@ -26,6 +29,8 @@ resource "aws_instance" "example" {
   }
 }
 
+# Create a security group for your EC2 instances
+# As a dependency, the creation of the SG precedes the EC2 instances
 resource "aws_security_group" "instance" {
   name = "tf-example-instance"
 
@@ -48,11 +53,14 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+# Create an output 'variable' that can be logged to std output after apply
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
 
+# Create a Launch configuration
+# NB: AWS has since introduced Launch templates in place of Launch configurations
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0e742cca61fb65051"
   instance_type   = "t2.medium"
@@ -63,15 +71,19 @@ resource "aws_launch_configuration" "example" {
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  # Required whn using a launch configuration with an autoscaling group
+  # Required when using a launch configuration with an autoscaling group
   lifecycle {
     create_before_destroy = true
   }
 }
 
+# Create an ASG resource
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -92,5 +104,87 @@ data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+
+# Create a load balancer resource
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# Create a load balancer listener resource
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = var.server_port
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# Create security group specific to the ALB
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a target group for your ASG
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Create listener rules for the ALB
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
